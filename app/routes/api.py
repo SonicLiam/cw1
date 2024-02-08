505d24b2fbf1b55f1b4a255d68e7e8b4667ba156
Sol-苏晨阳 <sol@fanruan.com>
2024-02-08 12:20:25 +0800

    add PUT and DELETE apis, support GET query with id and year param

--- app/routes/api.py
+++ app/routes/api.py
@@ -15,9 +15,18 @@ occupation_schema = OccupationSchema()
 
 # Common function to create API routes
 def create_endpoints(blueprint, url, endpoint_suffix, model, schema):
+    @blueprint.route(f'{url}/<int:id>', methods=['GET'])
+    def get_item(id):
+        item = model.query.get_or_404(id)
+        return jsonify(schema.dump(item)), 200
+
     @blueprint.route(url, methods=['GET'])
     def get_items():
-        items = model.query.all()
+        year = request.args.get('year', type=int)
+        if year:
+            items = model.query.filter_by(year=year).all()
+        else:
+            items = model.query.all()
         return jsonify(schema.dump(items, many=True)), 200
 
     @blueprint.route(url, methods=['POST'])
@@ -28,11 +37,36 @@ def create_endpoints(blueprint, url, endpoint_suffix, model, schema):
         db.session.commit()
         return schema.jsonify(item), 201
 
-    get_items.__name__ = f'get_{endpoint_suffix}'
-    create_item.__name__ = f'create_{endpoint_suffix}'
+    # PUT
+    @blueprint.route(f'{url}/<int:id>', methods=['PUT'])
+    def update_item(id):
+        item = model.query.get_or_404(id)
+        data = request.get_json()
+        schema.load(data, instance=item)
+        db.session.commit()
+        return schema.jsonify(item), 200
 
+    # DELETE
+    @blueprint.route(f'{url}/<int:id>', methods=['DELETE'])
+    def delete_item(id):
+        item = model.query.get_or_404(id)
+        db.session.delete(item)
+        db.session.commit()
+        return '', 204
+
+    # Set the function names
+    get_item.__name__ = f'get_{endpoint_suffix}'
+    get_items.__name__ = f'get_{endpoint_suffix}s'
+    create_item.__name__ = f'create_{endpoint_suffix}'
+    update_item.__name__ = f'update_{endpoint_suffix}'
+    delete_item.__name__ = f'delete_{endpoint_suffix}'
+
+    # Register the routes
+    blueprint.add_url_rule(f'{url}/<int:id>', view_func=get_item, methods=['GET'])
     blueprint.add_url_rule(url, view_func=get_items, methods=['GET'])
     blueprint.add_url_rule(url, view_func=create_item, methods=['POST'])
+    blueprint.add_url_rule(f'{url}/<int:id>', view_func=update_item, methods=['PUT'])
+    blueprint.add_url_rule(f'{url}/<int:id>', view_func=delete_item, methods=['DELETE'])
 
 
 # Register API routes
